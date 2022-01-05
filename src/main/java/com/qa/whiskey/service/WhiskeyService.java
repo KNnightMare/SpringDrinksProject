d690dfb75a012347314418fd1e03fe0fcc8010d1
KNnightMare <denisbozhilov@gmail.com>
2022-01-05 14:50:58 +0000

    Done my resources

---
+++ src/main/java/com/qa/whiskey/service/WhiskeyService.java
@@ -0,0 +1,42 @@
+package com.qa.whiskey.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.qa.whiskey.domain.Whiskey;
+
+@Service
+public class WhiskeyService implements ServiceMethods <Whiskey> {
+
+	@Override
+	public Whiskey create(Whiskey t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Whiskey> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Whiskey getById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Whiskey update(long id, Whiskey t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
+}
