c54b3d3790ed929685a4961fe780be25cb846659
KNnightMare <denisbozhilov@gmail.com>
2022-01-05 14:40:26 +0000

    lets start

---
+++ src/test/java/com/qa/whiskey/SpringDrinksProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.whiskey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDrinksProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
