c54657b7769a3e5fa1a4cfc98a881c17bc3c08c3
KNnightMare <denisbozhilov@gmail.com>
2022-01-07 12:49:02 +0000

    Finished with testing the domain entity class

--- src/test/java/com/qa/whiskey/service/WhiskeyServiceUnitTest.java
+++ src/test/java/com/qa/whiskey/service/WhiskeyServiceUnitTest.java
@@ -2,7 +2,7 @@ package com.qa.whiskey.service;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
